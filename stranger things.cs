fbd7be54483cec5a6eddcdff942c741b3b307383
Nicolle09 <nicollezmonsalve@gmail.com>
2024-05-16 15:31:34 -0500

    programa para registrar las criaturas
    completar este programa para registrar las criaturas como el  demogorgon.

---
+++ stranger things.cs
@@ -0,0 +1,13 @@
+/******************************************************************************
+autor: nicolle zapata - Any michelle 
+fecha: 2024-05-16
+*******************************************************************************/
+using System;
+class Program
+{
+      static void Main(string[] args)
+      {
+       string nombreCriatura = "Demogorgon";
+       Console.WriteLine("el nombre de la criatura es " + nombreCriatura );
+  }
+}
